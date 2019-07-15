334678af296951516c7ab0a7ee780f451607ed61
Hector <hmtv16@gmail.com>
2019-07-15 16:07:58 -0500

    Develop ckeckSlide function

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  sliderImages.forEach(sliderImages => {
+    //   half way through the image
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImages.height / 2;
+    //   bottom of the image
+    const imageBottom = sliderImages.offsetTop + sliderImages.height;
+    const isHalfShown = slideInAt > sliderImages.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImages.classList.add('active');
+    } else {
+      sliderImages.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
